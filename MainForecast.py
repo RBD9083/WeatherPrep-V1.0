6aa1419b4bd6fc7dec362a609f182fb103973a02
RafaelBD060 <139974701+RBD9083@users.noreply.github.com>
2025-10-21 23:07:13 +0700

    Add files via upload
    Transfer from local(main)

---
+++ MainForecast.py
@@ -0,0 +1,102 @@
+
+import requests #Agar bisa terhubung ke API
+from fAPI_KEY import vAPI_KEY #Mengambil kunci api
+import sys #Untuk keluar jika eror 
+
+def MintaData(kota):
+    #membuat url berdasarkan input kota pengguna
+    url = f'https://www.meteosource.com/api/v1/free/point?place_id={kota}&sections=hourly&language=en&units=metric&key={vAPI_KEY}'
+    try:
+        hasil = requests.get(url)
+    except: #Berarti internet gak nyambung
+        print("Periksa kondisi internet")
+        main()
+    if hasil.status_code == 200: #Kode 200 berarti sukses
+        data=hasil.json() #memformat data
+        return data
+    elif hasil.status_code == 403: #API KEY invalid
+        print("Pastikan sudah membuat folder dan variabel kunci API dan mengimportnya ke MainForecast.py")
+        sys.exit(1)
+    elif hasil.status_code == 402 or hasil.status_code == 429: #Jatah sudah habis
+        print('Jatah penggunaan cuaca hari/menit ini sudah habis, mohon maaf. Tunggu sebentar/esok hari untuk menggunakannya kembali')
+        sys.exit(1)
+    elif hasil.status_code == 400: #Nama kota salah/tidak support
+        print("Nama kota tidak resmi atau Kota tidak ada datanya di API")
+        main()
+    
+    
+def GarisBesarCuaca3jam(Data): #Memproses data dan mengembalikan antara "Badai""Terik","Berawan","Hujan" dari data 3 jam pertama
+    
+    Prakiraan3Jam = [jam['weather'] for jam in Data['hourly']['data'][:3]] #Hanya mengambil data 'cuaca' di 3 jam pertama
+    if any('thunderstorm' == cuaca for cuaca in Prakiraan3Jam): #badai
+        return 'BADAI'
+    elif any('rain' == cuaca for cuaca in Prakiraan3Jam): #hujan
+        return 'HUJAN'
+    elif any(cuaca in ['cloudy', 'overcast'] for cuaca in Prakiraan3Jam): #berawan
+        return 'BERAWAN'
+    else:
+        return "CERAH" #selain itu berarti cerah
+    
+def MembuatRekomendasi(cuaca):
+    if cuaca == 'BADAI':
+        return """PERINGATAN: Batalkan rencana jika tidak terlalu penting/mendesak. Jika kebutuhan memang mendesak, maka:
+                  -SANGAT DIREKOMENDASIKAN menggunakan kendaraan mobil agar terlindung dari angin dan hujan deras badai
+                  -Cek kondisi mobil seperti tekanan ban, aki, bahan bakar penuh
+                  -Pakailah pakaian yang hangat mengantisipasi suhu dingin (Jika terpaksa memakai motor, pakailah jaket anti air)
+                  -Bawa selimut dan senter darurat, P3K sederhana, power bank + charger dalam tas kedap air
+                  -Gunakanlah sepatu anti-licin
+                  -Ingatlah untuk memprioritaskanlah keselamatan
+                """
+    elif cuaca == 'HUJAN':
+        return """-Gunakan kendaraan mobil jika memungkinkan, nyalakan lampu, dan pastikan wiper berfungsi baik
+                  -Jika memakai motor, gunakan jas hujan yang tertutup rapat serta helm dengan visor bening
+                  -Pakailah pakaian yang cepat kering atau anti air, hindari bahan tebal yang menyerap air
+                  -Bawalah payung atau jas hujan cadangan, serta tas kedap air untuk melindungi barang elektronik
+                  -Gunakan sepatu tertutup agar kaki tetap kering dan aman dari jalanan licin
+                  -Siapkan powerbank dan plastik pelindung untuk HP atau dokumen penting"
+                """
+    elif cuaca == 'BERAWAN':
+        return """
+                -Gunakan kendaraan seperti biasa namun tetap waspada jika terlihat tanda-tanda hujan
+                -Pakailah pakaian yang nyaman dengan lapisan tipis seperti jaket ringan atau sweater
+                -Bawalah payung lipat atau jas hujan ringan sebagai antisipasi perubahan cuaca mendadak
+                -Siapkan botol minum secukupnya agar tubuh tetap terhidrasi
+                -Gunakan sepatu yang nyaman untuk berjalan dan tidak licin jika jalan mulai basah"
+                """
+    elif cuaca == 'CERAH':
+        return """-Gunakan kendaraan yang memiliki ventilasi atau AC yang baik
+                  -Pakailah pakaian berbahan ringan dan menyerap keringat, hindari pakaian gelap yang menyerap panas
+                  -Gunakan tabir surya (sunscreen) dan kacamata hitam untuk melindungi kulit serta mata
+                  -Bawalah topi atau payung untuk perlindungan dari sinar matahari langsung
+                  -Siapkan botol air minum untuk mencegah dehidrasi, bisa ditambah elektrolit jika perjalanan panjang
+                  -Gunakan alas kaki yang nyaman agar tidak mudah berkeringat dan tetap aman saat bepergian"
+                """
+    else:
+        print("WEATHER nilainya tidak masuk perkiraan")
+        sys.exit(1)
+
+
+def main():
+    print('Selamat datang di program rekomendasi persiapan berbasis prakiraan cuaca')
+    print('\n')
+    try: #meminta input kota dengan error handling
+        Kota = str(input('Masukkan nama kota dengan nama resminya (masukkan Yogyakarta, bukan jogja): ')).lower()
+    except:
+        print("Masukkan nama kota dengan benar")
+        main()
+    DataMentah = MintaData(Kota) #meminta prakiraan cuaca dengan API Meteosource
+    print("Meminta prakiraan cuaca ke API....")
+    Cuaca = GarisBesarCuaca3jam(DataMentah) #Meyimpulkan cuaca jam kedepan
+    Rekomendasi = MembuatRekomendasi(Cuaca) #Membuat rekomendasi persiapan
+    Hasil= f'Berdasarkan data, Cuaca {Kota} dalam 3 jam diprakirakan {Cuaca}, Kita merekomendasikan user:\n{Rekomendasi}' #Menambah pembukaan default
+    print(Hasil, '\n\n') #Memberikan hasil
+    Lagi = input("Mau menggunakan program lagi? (Y/N): ")
+    if Lagi == 'Y':
+        main()
+    elif Lagi == 'N':
+        print('Terimakasih telah menggunakan program')
+        sys.exit(1)
+    else:
+        sys.exit(1)
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,87 @@
+# WeatherPrep 1.0:Program Python Memberikan rekomendasi bepergian sebelum bepergian dengan prakiraan cuaca 3 jam kedepan
+Program Python ini memberikan rekomendasi kendaraan, pakaian, dan barang bawaan yang relevan untuk membantu pengguna mempersiapkan diri sebelum bepergian berdasarkan prakiraan cuaca di kota pengguna (dari input pengguna).
+Data Prakiraan Cuaca didapatkan dari API Meteosource (https://www.meteosource.com) free plan(gratis)
+
+# Cara penggunaan
+-Jalankan program python dengan terminal "python MainForecast.py"
+-Masukkan nama kota sesuai nama resmi, contoh: 'Yogyakarta' bukan 'jogja'
+
+# Contoh
+-Input: "Yogyakarta"
+-Output:"""-Gunakan kendaraan mobil jika memungkinkan, nyalakan lampu, dan pastikan wiper berfungsi baik
+                  -Jika memakai motor, gunakan jas hujan yang tertutup rapat serta helm dengan visor bening
+                  -Pakailah pakaian yang cepat kering atau anti air, hindari bahan tebal yang menyerap air
+                  -Bawalah payung atau jas hujan cadangan, serta tas kedap air untuk melindungi barang elektronik
+                  -Gunakan sepatu tertutup agar kaki tetap kering dan aman dari jalanan licin
+                  -Siapkan powerbank dan plastik pelindung untuk HP atau dokumen penting"
+                """
+# Batasan:
+-Hanya Kota yang ada di data Meteosource yang bisa diprakirakan
+-Perekomendasian terbatas pada 4 cuaca yaitu Sunny/Terik,Cloudy/Berawan,Rain/Hujan, Thunderstorm/Badai
+-Karna API menggunakan Free Plan, setiap hari dibatasi 400 request, 1 prakiraan memakan 1 request
+
+# Algoritma
+-Minta input nama kota resmi(Jakarta/Jakarta/dll), verifikasi agar benar
+-Meminta data prakiraan cuaca di kota input dari API meteosource
+-Mengambil data 3 jam kedepan dan menyimpulkan cuaca secara garis besar
+-memberi output berupa rekomendasi persiapan
+-menanyakan penggunaan ulang progam(Y/N)
+# Spesifikasi
+-Bahasa pyhton versi 3.8+
+-library eksternal 'request'
+-membutuhkan kunci API meteosource (free plan) yang disimpan di 'fAPI_KEY.py' dengan nama variabel 'vAPI_KEY'
+-Sistem operasi windows/macOS versi 8.1+/10.9 Mavericks+
+-Koneksi internet untuk API
+# Error Handling
+Input bukan string -> pesan plus ulangi program
+Intenet putus -> memberi pesan, ulangi program
+Kunci APi salah -> memberi pesan, hentikan program
+Kouta API habis -> memberi pesan, hentikan program
+Kota salah ejaan/gak ada di data API -> memberi pesan, ulangi program
+
+# Pseudo-code:
+IMPOR API_KEY,request,dll
+FUNCTION MintaData(kota):
+    buat URL API menggunakan kota dan API_KEY
+    coba kirim request HTTP GET
+        jika gagal koneksi -> tampilkan "Periksa internet" dan panggil main()
+    jika status_code 200 -> kembalikan data JSON
+    jika 403 -> tampilkan error API KEY, keluar
+    jika 402/429 -> tampilkan kuota habis, keluar
+    jika 400 -> tampilkan kota tidak valid, kembali ke main()
+
+FUNCTION GarisBesarCuaca3jam(Data):
+    ambil 3 data pertama dari 'hourly'
+    jika ada 'thunderstorm' -> kembalikan BADAI
+    jika ada 'rain' -> kembalikan HUJAN
+    jika ada 'cloudy' atau 'overcast' -> kembalikan BERAWAN
+    selain itu -> CERAH
+
+FUNCTION MembuatRekomendasi(cuaca):
+    jika BADAI -> tampilkan rekomendasi darurat
+    jika HUJAN -> tampilkan rekomendasi hujan
+    jika BERAWAN -> rekomendasi antisipasi
+    jika CERAH -> rekomendasi cuaca panas
+
+FUNCTION main():
+    minta input kota dari user
+    panggil MintaData dengan parameter kota
+    panggil GarisBesarCuaca3jam dengan parameter data
+    panggil MembuatRekomendasi dengan parameter Garisbesar cuaca
+    tampilkan hasil
+    tanya mau ulang atau keluar
+
+END
+
+# Set-Up program (1 kali)
+-Install python versi 3.8+
+-Install library request
+-Di folder program, buat file 'fAPI_KEY.py' dengan isi variabel string 'vAPI_KEY' berisi kunci API Meteosource
+
+# Versi & Pengembang
+-WeatherPrep v1.0
+-Pengembang:RBD9083 at Github
+-Lisensi:MIT License
+
+# Link Repository di GitHub
+-
